ffa6e0b9526fa4d78e4a51ffdd90c60802a00263
vipin-k <vipinkumargupta@gmail.com>
2018-03-30 08:19:18 -0700

    v2.0
    v2.0

---
+++ rdp.ps1
@@ -0,0 +1,2 @@
+﻿(Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1)
+(Get-WmiObject -Class "Win32_TSGeneralSetting" -Namespace root\cimv2\TerminalServices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
